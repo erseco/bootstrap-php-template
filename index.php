498a96956276499cea43f7c52a9f67ee2f3764cc
Ben Bristow <me@benbristow.co.uk>
2012-11-04 21:56:47 +0000

    Add page title

--- index.php
+++ index.php
@@ -2,20 +2,26 @@
 $page = $_GET['page'];
 
 //Page logic
-include('view/header.phtml');
 
 if(empty($page))
 {
+        $page = "home";
+        $title = "Home";
+        include('view/header.phtml');    
 	include('view/home.phtml');
 }
 else
 {
 	if(!file_exists("view/" . $page . ".phtml"))
 	{
+                $title = "404";
+                include('view/header.phtml');
 		include('view/404.phtml');
 	}
 	else
 	{
+                $title = ucfirst($page);
+                include('view/header.phtml');
 		include('view/' . $page . '.phtml');
 	}
 }
